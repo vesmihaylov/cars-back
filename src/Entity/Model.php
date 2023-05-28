e6a43b5a8261f560e166224daf3e68201a6badbc
Veselin Mihaylov <vesmihaylovdev@gmail.com>
2023-05-28 15:07:13 +0300

    feat(Model): allow filter by brand id

--- src/Entity/Model.php
+++ src/Entity/Model.php
@@ -2,23 +2,28 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\GetCollection;
 use App\Repository\ModelRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Uid\Uuid;
 
 #[ORM\Entity(repositoryClass: ModelRepository::class)]
 #[ORM\Table(name: 'brand_models')]
-#[ApiResource]
-#[GetCollection]
+#[ApiResource(paginationEnabled: false)]
+#[GetCollection(normalizationContext: ['groups' => ['models:read']])]
+#[ApiFilter(SearchFilter::class, properties: ['brand' => 'exact'])]
 class Model
 {
     #[ORM\Id]
     #[ORM\Column(type: UuidType::NAME, unique: true)]
     #[ORM\GeneratedValue(strategy: 'CUSTOM')]
     #[ORM\CustomIdGenerator(class: 'doctrine.uuid_generator')]
+    #[Groups(['models:read'])]
     private ?Uuid $id = null;
 
     #[ORM\ManyToOne(inversedBy: 'models')]
@@ -26,6 +31,7 @@ class Model
     private ?Brand $brand = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['models:read'])]
     private ?string $name = null;
 
     public function getId(): ?Uuid
